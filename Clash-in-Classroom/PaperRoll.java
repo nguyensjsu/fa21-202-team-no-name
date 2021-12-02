630e9a5e98bd79e8e564d5c51e97c60c28e63487
Shadow279 <hhhand123@gmail.com>
2021-12-02 14:20:28 -0800

    Added paper roll

---
+++ Clash-in-Classroom/PaperRoll.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PaperRoll here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PaperRoll extends GameActors
+{
+    /**
+     * Act - do whatever the PaperRoll wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
