e5f89a4961ada2a76332eb327a97fdc45d33ae70
mercury297 <chinmay.kamerkar@somaiya.edu>
2021-12-02 20:46:03 -0800

    feat: add observer pattern interfaces

---
+++ Clash-in-Classroom/Match.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class Match here.
+ * 
+ * @author (Chinmay Kamerkar) 
+ * @version (a version number or a date)
+ */
+public interface Match  
+{
+    public void notifyObservers();
+}
---
+++ Clash-in-Classroom/Observer.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class Observer here.
+ * 
+ * @author (Chinmay Kamerkar) 
+ * @version (a version number or a date)
+ */
+public interface Observer  
+{
+    public void updateScoreCard(GameActors subject);
+}
