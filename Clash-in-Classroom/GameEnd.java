002c8345123cfbd642c44aa48ac65c11b034ef64
mercury297 <chinmay.kamerkar@somaiya.edu>
2021-12-03 12:30:14 -0800

    feat: add object for ending game

---
+++ Clash-in-Classroom/GameEnd.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameEnd here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameEnd extends Actor
+{
+    /**
+     * Act - do whatever the GameEnd wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    public GameEnd(String message) 
+    {
+        setImage(new GreenfootImage(message, 30, Color.WHITE, Color.BLACK));
+    }   
+}
