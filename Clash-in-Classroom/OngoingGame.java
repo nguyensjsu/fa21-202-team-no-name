c47ab73aed8996ae561e3b0f69bc4c0af2aed2b1
Wen-Hao Tseng <hao70290@gmail.com>
2021-12-03 20:05:28 -0800

    refactor: Updated game messages

--- Clash-in-Classroom/OngoingGame.java
+++ Clash-in-Classroom/OngoingGame.java
@@ -11,7 +11,7 @@ public class OngoingGame extends GameState
     @Override
        public void doWin() {
             my_world.setState(my_world.getWinState());
-            GameEnd gameEnd = new GameEnd("Yay! You won!");
+            GameEnd gameEnd = new GameEnd("Hurray! You won with a score of 500!");
             my_world.addObject(gameEnd, my_world.getWidth()/2, my_world.getHeight()/2);
             Greenfoot.stop();
         }
@@ -19,7 +19,7 @@ public class OngoingGame extends GameState
     @Override
     public void doLose() {
         my_world.setState(my_world.getLoseState());
-        GameEnd gameEnd = new GameEnd("Yay! You lose!");
+        GameEnd gameEnd = new GameEnd("Sorry! You are out of GUMBALLS... Oops... Paper Balls!");
         my_world.addObject(gameEnd, my_world.getWidth() / 2, my_world.getHeight() / 2);
         Greenfoot.stop();
     }
