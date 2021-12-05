b84d6540286a337c658ea9672d6e9ea0b8a6f016
mercury297 <chinmay.kamerkar@somaiya.edu>
2021-12-04 17:29:43 -0800

    clean

--- Clash-in-Classroom/GameEnd.java
+++ Clash-in-Classroom/GameEnd.java
@@ -2,15 +2,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * GameEnd class displays message on screen at the end of game
- * @author (your name) 
+ * @author Chinmay 
  * @version (a version number or a date)
  */
 public class GameEnd extends Actor
 {
-    /**
-     * Act - do whatever the GameEnd wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         // Add your action code here.
