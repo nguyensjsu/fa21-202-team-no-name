890c23ef32f5947babe3bde2cdcd36286bc51846
mercury297 <chinmay.kamerkar@somaiya.edu>
2021-12-04 17:17:25 -0800

    feat: add javadoc comments

--- Clash-in-Classroom/ScoreCardDisplay.java
+++ Clash-in-Classroom/ScoreCardDisplay.java
@@ -1,7 +1,7 @@
 import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class ScoreCardDisplay here.
+ * ScoreCardDisplay class displays the content of the ScoreCard on the GameWorld
  * 
  * @author (Chinmay Kamerkar)
  * @version (a version number or a date)
@@ -18,6 +18,10 @@ public class ScoreCardDisplay extends Actor {
         // Add your action code here.
     }
 
+    /**
+    * Returns the singleton instance of ScoreCardDisplay 
+    * @return instance
+    */
     public static ScoreCardDisplay getInstance() {
         if (instance == null) {
             GreenfootImage paperBallImage = new GreenfootImage("PaperRollSmall.png");
@@ -30,12 +34,29 @@ public class ScoreCardDisplay extends Actor {
     public ScoreCardDisplay() {
     }
 
+
+    /**
+    * This is parametrized of constructor of ScoreCardDisplay which will create an instance of ScoreCardDisplay using ScoreCard singleton
+    * @param ballsLeft number of balls left
+    * @param image GreenfootImage of paperroll
+    * @return
+    * @see GreenfootImage
+    */
     public ScoreCardDisplay(int ballsLeft, GreenfootImage image) {
         // background = getImage();
         background = getImage();
         display(ScoreCard.getInstance().getPaperBallsLeft() - 1, ScoreCard.getInstance().getScore(), image);
     }
 
+
+    /**
+    * This method will set the image of ScoreCardDisplay class
+    * @param ballsLeft number of balls left
+    * @param score current score
+    * @param image GreenfootImage of paperroll
+    * @return
+    * @see GreenfootImage
+    */
     public void display(int ballsLeft, int score, GreenfootImage image) {
         background = getImage();
         // mainDisplay = new GreenfootImage(500, 100);
@@ -50,6 +71,10 @@ public class ScoreCardDisplay extends Actor {
         setImage(background);
     }
 
+    /**
+    * This method will update the image of ScoreCardDisplay class by changing the number of balls left and score
+    * @return
+    */
     public void updateDisplay() {
         //
         background.clear();
