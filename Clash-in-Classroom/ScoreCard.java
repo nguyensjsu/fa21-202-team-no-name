5e008567e8c19ad0577e1fa0168670aa8b2f6e31
mercury297 <chinmay.kamerkar@somaiya.edu>
2021-12-04 17:17:14 -0800

    feat: add javadoc comments

--- Clash-in-Classroom/ScoreCard.java
+++ Clash-in-Classroom/ScoreCard.java
@@ -1,7 +1,7 @@
 import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class ScoreCard here.
+ * ScoreCard class is an Observer of PaperRoll and gets updated when PaperRoll is thrown
  * 
  * @author (Chinmay Kamerkar)
  * @version (a version number or a date)
@@ -10,6 +10,10 @@ public class ScoreCard extends Actor implements Observer {
     private static int numPaperBalls = 8;
     private static int score = 0;
     private static ScoreCard instance;
+    /**
+    * Returns the singleton instance of ScoreCard 
+    * @return instance
+    */
     public static ScoreCard getInstance() {
         if (instance == null) {
             return new ScoreCard();
@@ -17,23 +21,43 @@ public class ScoreCard extends Actor implements Observer {
         return instance;
     }
 
+    /**
+    * Returns the number of paper balls left 
+    * @return  numPaperBalls
+    */
     public int getPaperBallsLeft() {
         return numPaperBalls;
     }
 
+    /**
+    * Reduces the number of paper balls by 1
+    * @return
+    */
     public void setPaperBalls() {
         if (numPaperBalls > 0)
             numPaperBalls -= 1;
     }
 
+    /**
+    * Increases the score by 100
+    * @return
+    */
     public void setScore() {
         score += 100;
     }
 
+    /**
+    * Returns the current score
+    * @return score
+    */
     public int getScore() {
         return score;
     }
 
+    /**
+    * Resets the current score and number of paper balls
+    * @return
+    */
     public void reset_score() {
         numPaperBalls = 8;
         score = 0;
@@ -43,10 +67,18 @@ public class ScoreCard extends Actor implements Observer {
         // Add your action code here.
     }
 
+    /**
+    * reduces the number of paper balls of singleton instance by count 1
+    * @return
+    */
     public void updateBallsLeft() {
         ScoreCard.getInstance().setPaperBalls();
     }
 
+    /**
+    * Increases the current score of singleton instance by count 100
+    * @return
+    */
     public void updateScore() {
         ScoreCard.getInstance().setScore();
     }
